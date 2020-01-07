a0facbe2e213ba1c2804cc3cf17a7f340fe04705
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2020-01-07 14:38:36 +0900

    【復習回】P社の昼休み（#006〜#010）

---
+++ virtual-contests/pepabo/003/abc104/B.rb
@@ -0,0 +1,7 @@
+# https://atcoder.jp/contests/abc104/tasks/abc104_b
+
+
+
+S = gets.chomp
+
+puts S =~ /^A[a-z]+C[a-z]+$/ ? 'AC' : 'WA'
---
+++ virtual-contests/pepabo/003/abc106/A.rb
@@ -0,0 +1,7 @@
+# https://atcoder.jp/contests/abc106/tasks/abc106_a
+
+
+
+A, B = gets.split.map(&:to_i)
+
+puts (A - 1) * (B - 1)
