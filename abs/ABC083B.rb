e99070525dd58c92a533329597223c91e82f279d
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2020-01-25 08:14:19 +0900

    abc083 B を解いた

--- abs/ABC083B.rb
+++ abs/ABC083B.rb
@@ -4,4 +4,10 @@
 
 N, A, B = gets.split.map(&:to_i)
 
+ans = 0
+1.upto(N) do |i|
+  sum = i.to_s.chars.inject(0) { |result, char| result + char.to_i }
+  ans += i if sum >= A && sum <= B
+end
+
 puts ans
