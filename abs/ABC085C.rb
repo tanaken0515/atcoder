4366ce609b77741ae0bdf94324cb94287e7c5d78
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2020-01-27 08:42:29 +0900

    abc085 C を解いた

--- abs/ABC085C.rb
+++ abs/ABC085C.rb
@@ -4,4 +4,19 @@
 
 N, Y = gets.split.map(&:to_i)
 
-puts ans
+# 最小枚数となる組み合わせ
+x_max = Y / 10000
+(0..x_max).each do |x|
+  y_max = (Y - 10000 * x) / 5000
+  (0..y_max).each do |y|
+    z = N - x - y
+    break if z < 0
+
+    if 10000 * x + 5000 * y + 1000 * z == Y
+      puts "#{x} #{y} #{z}"
+      exit
+    end
+  end
+end
+
+puts '-1 -1 -1'
