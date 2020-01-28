e6eb1cf0f113b1372d6f995721f7ea5f6fb9be49
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2020-01-28 09:28:32 +0900

    abc087 B を解いた

--- abs/ABC087B.rb
+++ abs/ABC087B.rb
@@ -7,4 +7,15 @@ B = gets.to_i
 C = gets.to_i
 X = gets.to_i
 
+ans = 0
+y500 = [X / 500, A].min
+(0..y500).each do |a|
+  y100 = [(X - 500 * a) / 100, B].min
+  (0..y100).each do |b|
+    if (X - 500 * a - 100 * b) / 50 <= C
+      ans += 1
+    end
+  end
+end
+
 puts ans
