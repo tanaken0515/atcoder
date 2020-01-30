283ee7980d691939a370274e689326352d57cddd
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2020-01-31 08:08:07 +0900

    setup abc153

---
+++ abc153/B.rb
@@ -0,0 +1,8 @@
+# https://atcoder.jp/contests/abc153/tasks/abc153_b
+
+
+
+H, N = gets.split.map(&:to_i)
+As = gets.split.map(&:to_i)
+
+puts cond ? 'Yes' : 'No'
---
+++ abc153/C.rb
@@ -0,0 +1,8 @@
+# https://atcoder.jp/contests/abc153/tasks/abc153_c
+
+
+
+N, K = gets.split.map(&:to_i)
+Hs = gets.split.map(&:to_i)
+
+puts ans
