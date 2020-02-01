0f6a0ba92dfe1181b3a4ad7c9ef1d704ee669b68
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2020-02-01 09:16:46 +0900

    abc049 C を解いた

--- language-test-202001/ABC049C.rb
+++ language-test-202001/ABC049C.rb
@@ -1,7 +1,22 @@
 # https://atcoder.jp/contests/language-test-202001/tasks/arc065_a
 
 
+class String
+  def cut_tail!(tail)
+    if self[(self.length - tail.length)...(self.length)] == tail
+      self.replace(self[0...(self.length - tail.length)])
+    end
+  end
+end
 
-S = gets.chomp
+s = gets.chomp
+
+cond = true
+while s != ''
+  unless s.cut_tail!('dream') || s.cut_tail!('dreamer') || s.cut_tail!('erase') || s.cut_tail!('eraser')
+    cond = false
+    break
+  end
+end
 
 puts cond ? 'YES' : 'NO'
