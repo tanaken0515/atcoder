009a6a18e9c055654a3d56181d96fe1b0965e958
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2020-02-02 10:03:01 +0900

    abc126 B を解いた

--- abc126/B.rb
+++ abc126/B.rb
@@ -1,7 +1,22 @@
 # https://atcoder.jp/contests/abc126/tasks/abc126_b
 
+def month?(num)
+  num > 0 && num < 13
+end
 
 
 S = gets.to_i
 
+x = S / 100
+y = S % 100
+
+ans = if month?(x) && month?(y)
+        'AMBIGUOUS'
+      elsif month?(y)
+        'YYMM'
+      elsif month?(x)
+        'MMYY'
+      else
+        'NA'
+      end
 puts ans
