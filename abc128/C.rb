68fd3509422db01abedcd03e1360c3c360c47518
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2020-02-06 09:47:00 +0900

    abc128 C を解いた

--- abc128/C.rb
+++ abc128/C.rb
@@ -3,8 +3,30 @@
 MOD = 2
 
 N, M = gets.split.map(&:to_i)
-k_1, s_11, s_12, s_1k_1 = gets.split.map(&:to_i)
-k_M, s_M1, s_M2, s_Mk_M = gets.split.map(&:to_i)
+bulbs_with_switches = []
+M.times do
+  bulbs_with_switches << gets.split.map(&:to_i)
+end
 ps = gets.split.map(&:to_i)
+
+ans = 0
+(0..(2 ** N - 1)).each do |i|
+  on_off_list = format("%0#{N}d", i.to_s(2)).chars
+
+  off_switch_ids = []
+  on_off_list.each_with_index do |flg, idx|
+    off_switch_ids << idx + 1 if flg == '0'
+  end
+
+  all_lighting = true
+  bulbs_with_switches.each_with_index do |bulb_with_switches, idx|
+    unless (bulb_with_switches[1..-1] - off_switch_ids).size % 2 == ps[idx]
+      all_lighting = false
+      break
+    end
+  end
+
+  ans += 1 if all_lighting
+end
 
 puts ans
