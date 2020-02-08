b57336af1eeff03978e93682fd536a639884e91f
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2020-02-09 08:26:58 +0900

    setup abc130

---
+++ abc130/A.rb
@@ -0,0 +1,7 @@
+# https://atcoder.jp/contests/abc130/tasks/abc130_a
+
+
+
+X, A = gets.split.map(&:to_i)
+
+puts ans
---
+++ abc130/C.rb
@@ -0,0 +1,7 @@
+# https://atcoder.jp/contests/abc130/tasks/abc130_c
+
+
+
+W, H, x, y = gets.split.map(&:to_i)
+
+puts ans
