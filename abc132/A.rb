130718a1b800ab472fb00c1f4adb1b8ce5714050
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2020-02-12 08:56:29 +0900

    abc132 C を解いた

---
+++ abc132/A.rb
@@ -0,0 +1,7 @@
+# https://atcoder.jp/contests/abc132/tasks/abc132_a
+
+
+
+S = gets.chomp
+
+puts cond ? 'Yes' : 'No'
---
+++ abc132/D.rb
@@ -0,0 +1,7 @@
+# https://atcoder.jp/contests/abc132/tasks/abc132_d
+
+MOD = 10**9+7
+
+N, K = gets.split.map(&:to_i)
+
+puts ans
