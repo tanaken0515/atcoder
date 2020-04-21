1ebdfc00fc61b39302e5aeb1936b65ceb2538e4e
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2020-04-21 13:38:57 +0900

    P社の昼休み

---
+++ virtual-contests/pepabo/016/abc053/B.rb
@@ -0,0 +1,7 @@
+# https://atcoder.jp/contests/abc053/tasks/abc053_b
+
+
+
+s = gets.chomp
+
+puts s.rindex('Z') - s.index('A') + 1
---
+++ virtual-contests/pepabo/016/abc057/A.rb
@@ -0,0 +1,7 @@
+# https://atcoder.jp/contests/abc057/tasks/abc057_a
+
+
+
+A, B = gets.split.map(&:to_i)
+
+puts (A + B) % 24
